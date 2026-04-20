0b473da519c41331f80e6cefe33d1d6f27525948
Akridev <akshayonlyemail@gmail.com>
2026-04-21 00:23:17 +0800

    added debug lines to code

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -1,4 +1,5 @@
 const serverless = require("serverless-http");
 const app = require ("../../index");
-
+console.log("APP is starting! ...")
 exports.handler = serverless(app);
+console.log("EXPORTED!")
